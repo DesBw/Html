b5ab3b3e4d4843df3fefcf6090bea8110f647cf8
DesBw <desalegnbw@gmail.com>
2024-10-17 13:55:07 +0300

    Some sample pages
    I am learning html using Odin project. I have now setup two html pages (based on the instructions); some sample images to be linked, and one JavaScript file.

---
+++ OdinHTML/scripts/test.js
@@ -0,0 +1,15 @@
+var cities = [
+  "Atlanta",
+  "Baltimore",
+  "Chicago",
+  "Denver",
+  "Los Angeles",
+  "Seattle",
+];
+var nums = [50, 51, 55]; //adds via variable
+var comb1 = cities.concat(1, 2, 3, 4, 5); // adds directly = #push
+var comb2 = cities.concat(nums);
+console.log(comb1);
+console.log(comb2);
+alert(comb1);
+
